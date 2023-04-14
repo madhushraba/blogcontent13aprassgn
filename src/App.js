a898ae28238db3c234a45c8fb8722f67fc10f731
Madhushraba <madhushraba21das@gmail.com>
2023-04-14 07:02:00 +0530

    frst cmmt

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//https://jsonplaceholder.typicode.com/posts
 
 import React from "react";
 import "./style.css";
@@ -32,7 +5,7 @@ import { useContext, useEffect } from "react";
 import { createContext, useReducer, useState } from "react";
 
 const reducer = (state, action) => {
-  if (action.type == "Postdata") {
+  if (action.type === "Postdata") {
     return action.payload;
   }
 };
